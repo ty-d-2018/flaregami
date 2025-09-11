c87bfd6bed0689b84281dd5879553e9fb2f2d8a2
ty-d-2018 <40513477+ty-d-2018@users.noreply.github.com>
2025-09-10 20:01:41 -0600

    The common tongue and common thought shares a blueprint in design. No matter what craft you are.
    There is a common language to describe a point upon space. Upon the material presented and recieved.

--- src/craft/teaching.rs
+++ src/craft/teaching.rs
@@ -1,2 +1,18 @@
+use super::terrain::{
+    Material,
+    sensory::Vector,
+};
 
-pub struct Blueprint;
+use std::collections::HashMap;
+
+pub struct Blueprint{
+    points: HashMap::<usize, Box::<dyn Vector>>,
+}
+
+impl Blueprint{
+    pub fn new() -> Blueprint{
+        Blueprint{
+            points: HashMap::new(),
+        }
+    }
+}
--- src/craft/terrain/sensory.rs
+++ src/craft/terrain/sensory.rs
@@ -1,40 +1,62 @@
 
 pub trait Vector{
     fn get_dimension(&self) -> (f32, f32, f32);
-    fn get_x(arrow: &Self) -> f32{
-        let (x, y, z) = arrow.get_dimension();
+    fn get_x(&self) -> f32{
+        let (x, y, z) = self.get_dimension();
         x
     }
-    fn get_y(arrow: &Self) -> f32{
-        let (x, y, z) = arrow.get_dimension();
+    fn get_y(&self) -> f32{
+        let (x, y, z) = self.get_dimension();
         y
     }
-    fn get_z(arrow: &Self) -> f32{
-        let(x, y, z) = arrow.get_dimension();        
+    fn get_z(&self) -> f32{
+        let(x, y, z) = self.get_dimension();        
         z
     }
-    fn get_xy(arrow: &Self) -> (f32, f32){
-        let(x, y, z) = arrow.get_dimension();
+    fn get_xy(&self) -> (f32, f32){
+        let(x, y, z) = self.get_dimension();
         (x, y)
     }
-    fn get_xz(arrow: &Self) -> (f32, f32){
-        let (x, y, z) = arrow.get_dimension();
+    fn get_xz(&self) -> (f32, f32){
+        let (x, y, z) = self.get_dimension();
         (x, z)
     }
-    fn get_yx(arrow: &Self) -> (f32, f32){
-        let (x, y, z) = arrow.get_dimension();
+    fn get_yx(&self) -> (f32, f32){
+        let (x, y, z) = self.get_dimension();
         (y, x)
     }
-    fn get_yz(arrow: &Self) -> (f32, f32){
-        let (x, y, z) = arrow.get_dimension();
+    fn get_yz(&self) -> (f32, f32){
+        let (x, y, z) = self.get_dimension();
         (y, z)
     }
-    fn get_zx(arrow: &Self) -> (f32, f32){
-        let (x, y, z) = arrow.get_dimension();
+    fn get_zx(&self) -> (f32, f32){
+        let (x, y, z) = self.get_dimension();
         (z, x)
     }
-    fn get_zy(arrow: &Self) -> (f32, f32){
-        let (x, y, z) = arrow.get_dimension();
+    fn get_zy(&self) -> (f32, f32){
+        let (x, y, z) = self.get_dimension();
         (z, y)
+    }
+}
+
+pub struct CommonPosition{
+    x: f32,
+    y: f32,
+    z: f32,
+}
+
+impl CommonPosition{
+    pub fn new(x: f32, y: f32, z: f32) -> CommonPosition{
+        CommonPosition{
+            x,
+            y,
+            z,
+        }
+    }
+}
+
+impl Vector for CommonPosition{
+    fn get_dimension(&self) -> (f32, f32, f32){
+        (self.x, self.y, self.z)
     }
 }
