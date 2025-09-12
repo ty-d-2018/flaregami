136ce6d733def9f83303d0c6e9313702a7f569c5
ty-d-2018 <40513477+ty-d-2018@users.noreply.github.com>
2025-09-11 22:40:43 -0600

    There are different aspects.
    There is the description of design of something. A representation.
    
    Then there is the actual material with the actual data.
    
    That is what I added in the Vector trait. And then updated CommonPosition implementation of Vector.

--- src/craft/terrain/sensory.rs
+++ src/craft/terrain/sensory.rs
@@ -4,9 +4,21 @@ use super::connection::{
     Edge, 
 };
 
+use std::rc::Rc;
+use std::cell::RefCell;
+
+type DynVector = dyn Vector;
+type Design = Rc::<DynVector>;
+type BoxedVector = Box::<DynVector>;
+type MutableVector = RefCell::<BoxedVector>;
+type Material = Rc::<MutableVector>;
+
 pub trait Vector{
     fn get_dimension(&self) -> (f32, f32, f32);
-    fn get_clone(&self) -> Box::<dyn Vector>;
+    fn get_clone(&self) -> BoxedVector;
+    fn get_design(&self) -> Design;
+    fn ref_cell_clone(&self) -> MutableVector; 
+    fn into_material(&self) -> Material;
     fn get_x(&self) -> f32{
         let (x, y, z) = self.get_dimension();
         x
@@ -66,8 +78,17 @@ impl Vector for CommonPosition{
     fn get_dimension(&self) -> (f32, f32, f32){
         (self.x, self.y, self.z)
     }
-    fn get_clone(&self) -> Box::<dyn Vector>{
+    fn get_clone(&self) -> BoxedVector{
         Box::new(self.clone())
+    }
+    fn get_design(&self) -> Design{
+        Rc::new(self.clone())
+    }
+    fn ref_cell_clone(&self) -> MutableVector{
+        RefCell::new(self.get_clone())
+    }
+    fn into_material(&self) -> Material{
+        Rc::new(self.ref_cell_clone())
     }
 }
 
