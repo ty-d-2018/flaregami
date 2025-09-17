8dc5a30f9d8672f11777bfc622b688822517a906
ty-d-2018 <40513477+ty-d-2018@users.noreply.github.com>
2025-09-16 20:49:27 -0600

    Delete these modules. I am compressing the structure. Perhaps I might include a new version of these in the future. Or not.

--- src/craft.rs
+++
@@ -1,2 +0,0 @@
-pub mod teaching;
-pub mod terrain;
--- src/fire.rs
+++
@@ -1,7 +0,0 @@
-use super::ephemeral::{ Ancestor };
-
-pub struct Flames;
-
-impl Flames{
-    pub fn show_vision(spirits: &Vec::<Ancestor>){}
-}
